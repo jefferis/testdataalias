5427127693a57b58a312dedf3650577d4a16b32f
Gregory Jefferis <jefferis@gmail.com>
2017-10-08 08:08:48 +0100

    init skeleton project

---
+++ R/landmarks.R
@@ -0,0 +1,19 @@
+#' @description \code{landmarks.2016} landmarks set for 2016 brain
+#' @docType data
+#' @name landmarks.2016
+#' @rdname landmarks
+NULL
+
+#' @description \code{landmarks.2017} landmarks set for 2017 brain
+#' @docType data
+#' @name landmarks.2017
+#' @rdname landmarks
+NULL
+
+#' Landmarks for transformations between different assemblies of a refernce
+#' brain.
+#' @description \code{landmarks} always refers to the latest landmark set
+#' @docType data
+#' @name landmarks
+NULL
+
