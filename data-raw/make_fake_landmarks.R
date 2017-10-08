82f7e749eb94335fe33dd91d7311ec4b34714d73
Gregory Jefferis <jefferis@gmail.com>
2017-10-08 08:18:03 +0100

    data-raw to make fake landmarks for testing

---
+++ data-raw/make_fake_landmarks.R
@@ -0,0 +1,6 @@
+landmarks.2016=matrix(rnorm(30), ncol=3)
+landmarks.2017=matrix(rnorm(30), ncol=3)
+landmarks=landmarks.2017
+
+devtools::use_data(landmarks.2016)
+devtools::use_data(landmarks.2017)
