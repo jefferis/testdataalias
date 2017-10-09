f5fce5bb893141c40449a8fa9a602f98379fb529
Gregory Jefferis <jefferis@gmail.com>
2017-10-09 19:18:14 +0100

    put name of object at end of roxygen block
    * this does improve the docs but doesn't stop the install error

--- R/landmarks.R
+++ R/landmarks.R
@@ -2,19 +2,21 @@
 #' @docType data
 #' @name landmarks.2016
 #' @rdname landmarks
-NULL
+"landmarks.2016"
 
 #' @description \code{landmarks.2017} landmarks set for 2017 brain
 #' @docType data
 #' @name landmarks.2017
 #' @rdname landmarks
-NULL
+"landmarks.2017"
 
 #' Landmarks for transformations between different assemblies of a refernce
 #' brain.
 #' @description \code{landmarks} always refers to the latest landmark set
 #' @docType data
 #' @name landmarks
-NULL
+"landmarks"
 
 landmarks <- testdataalias::landmarks.2017
+# latest_landmarks <- function() {testdataalias::landmarks.2017}
+# landmarks <- latest_landmarks()
